eb090384df136b2398b9077d3c0f7bb4959f06d2
Eli-2009 <elieliyarov18@gmail.com>
2024-08-31 16:57:36 +0400

    homework

---
+++ app.js
@@ -0,0 +1,93 @@
+let a = prompt('ayin nomresini daxil edin')
+
+// let a = 3
+
+// switch (a) {
+//     case 1:
+//         console.log('yanvar');
+//         break;
+//     case 2:
+//         console.log('fevral');
+//         break
+//     case 3:
+//         console.log('mart');
+//         break;
+//     case 4:
+//         console.log('aprel');
+//         break
+//     case 5:
+//         console.log('may');
+//         break;
+//     case 6:
+//         console.log('iyun');
+//         break
+//     case 7:
+//         console.log('iyul');
+//         break;
+//     case 8:
+//         console.log('avqust');
+//         break
+//     case 9:
+//         console.log('sentyabr');
+//         break;
+//     case 10:
+//         console.log('oktyabr');
+//         break
+//     case 11:
+//         console.log('noyabr');
+//         break;
+//     case 12:
+//         console.log('dekabr');
+//         break
+//     default:
+//         console.log('duzgun daxil edin');
+        
+// } 
+
+
+
+
+
+
+
+if (a === '1' || a === '01') {
+    console.log('yanvar');
+}
+else if (a === '2' || a === '02') {
+    console.log('fevral');
+}
+else if (a === '3' || a === '03') {
+    console.log('mart');
+}
+else if (a === '4' || a === '04') {
+    console.log('aprel');
+}
+else if (a === '5' || a === '05') {
+    console.log('may');
+}
+else if (a === '6' || a === '06') {
+    console.log('iyun');
+}
+else if (a === '7' || a === '07') {
+    console.log('iyul');
+}
+else if (a === '8' || a === '08') {
+    console.log('avqust');
+}
+else if (a === '9' || a === '09') {
+    console.log('sentyabr');
+}
+else if (a === '10') {
+    console.log('oktyabr');
+}
+else if (a === '11') {
+    console.log('noyabr');
+}
+else if (a === '12') {
+    console.log('dekabr');
+}
+
+else{
+    console.log('duzgun daxil edin');
+    
+}
